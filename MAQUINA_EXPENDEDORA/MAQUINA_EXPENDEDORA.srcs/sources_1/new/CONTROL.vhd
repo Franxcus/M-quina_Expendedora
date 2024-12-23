78f734a3612830885839b9f67ec41175e8ed222a
Jimenalopez02 <jimena.lopez@alumnos.upm.es>
2024-12-23 15:07:12 +0100

    MEJORA

---
+++ MAQUINA_EXPENDEDORA/MAQUINA_EXPENDEDORA.srcs/sources_1/new/CONTROL.vhd
@@ -0,0 +1,189 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 21.12.2023 17:51:11
+-- Design Name: 
+-- Module Name: DISPLAY_CONTROL - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+USE ieee.std_logic_arith.ALL;
+USE ieee.std_logic_unsigned.ALL;
+
+entity DISPLAY_CONTROL is
+    Generic(
+        TAM_CUENTA: POSITIVE;
+        NUM_TIPO_REFRESCOS: POSITIVE;
+        TAM_CODE: POSITIVE;
+        NUM_ESTADOS: POSITIVE;
+        NUM_DISPLAYS: POSITIVE
+    );
+    Port(
+        CUENTA : in STD_LOGIC_VECTOR (TAM_CUENTA - 1 downto 0);
+        TIPO_REFRESCO: in STD_LOGIC_VECTOR (NUM_TIPO_REFRESCOS - 1 downto 0);
+        PRECIOS: in std_logic_vector(NUM_TIPO_REFRESCOS * TAM_CUENTA - 1 downto 0);
+        CLK : in STD_LOGIC;
+        CODE : out STD_LOGIC_VECTOR (TAM_CODE * NUM_ESTADOS - 1 downto 0);
+        CONTROL : out STD_LOGIC_VECTOR (NUM_DISPLAYS * NUM_ESTADOS - 1 downto 0)
+    );
+end DISPLAY_CONTROL;
+
+architecture Behavioral of DISPLAY_CONTROL is
+
+-- SE DEFINEN VECTORES DE CODIGO Y CONTROL DE SEGMENTOS PARA CADA ESTADO
+subtype CONTROL_ESTADO is STD_LOGIC_VECTOR(NUM_DISPLAYS - 1 downto 0);
+subtype CODE_ESTADO is STD_LOGIC_VECTOR(TAM_CODE - 1 downto 0);
+
+-- SE DEFINE EL ARRAY QUE CONTENDR� TODOS LOS CONTROLES Y CODES DE LOS ESTADOS 
+type CONTROL_TOTAL is ARRAY (0 to NUM_ESTADOS - 1) of CONTROL_ESTADO;
+type CODE_TOTAL is ARRAY (0 to NUM_ESTADOS - 1) of CODE_ESTADO;
+
+-- CREAMOS LAS SE�ALES 
+signal CONTROL_SIG : CONTROL_TOTAL;
+signal CODE_SIG : CODE_TOTAL;
+
+begin
+
+process(CLK)
+begin
+    IF rising_edge (CLK) THEN 
+    
+        CASE CONTROL_SIG(0) IS -- PROD 1 / PROD 2
+            WHEN "111101111" =>
+                CONTROL_SIG(0) <= "111011111";
+                CODE_SIG(0) <= "10001"; -- LETRA D
+            WHEN "111011111" =>
+                CONTROL_SIG(0) <= "110111111";
+                CODE_SIG(0) <= "10101"; -- LETRA O
+            WHEN "110111111" =>
+                CONTROL_SIG(0) <= "101111111";
+                CODE_SIG(0) <= "10111"; -- LETRA R
+            WHEN "101111111" =>
+                CONTROL_SIG(0) <= "011111111";
+                CODE_SIG(0) <= "10110"; -- LETRA P
+            WHEN OTHERS =>
+                CONTROL_SIG(0) <= "111101111";
+                IF TIPO_REFRESCO = "01" THEN
+                    CODE_SIG(0) <= "00001"; -- NUMERO DEL PRODUCTO
+                ELSIF TIPO_REFRESCO = "10" THEN
+                    CODE_SIG(0) <= "00010"; -- NUMERO DEL PRODUCTO
+                ELSE CODE_SIG(0) <= "11111"; -- NINGUN PRODUCTO SELECCIONADO
+                END IF;          
+        END CASE;
+        
+        CASE CONTROL_SIG(1) IS -- MUESTRA EL PRECIO DEL REFRESCO Y LO QUE FALTA PARA PAGAR
+            WHEN "111111101" =>
+                CONTROL_SIG(1) <= "111111011";
+                -- MOSTRAMOS EL PRIMER DECIMAL DEL PRECIO QUE FALTA PARA PAGAR
+                -- SI FALTA POR PAGAR 90C, SACAR� POR PANTALLA UN 9
+                IF TIPO_REFRESCO = "01" THEN
+                    IF PRECIOS(TAM_CUENTA - 1 downto 0) - CUENTA >= "01010" THEN
+                        CODE_SIG(1) <= PRECIOS(TAM_CUENTA - 1 downto 0) - "01010" - CUENTA;
+                    ELSE 
+                        CODE_SIG(1) <= PRECIOS(TAM_CUENTA - 1 downto 0) - CUENTA;
+                    END IF;
+                 ELSIF TIPO_REFRESCO = "10" THEN
+                    IF PRECIOS((TAM_CUENTA*2) - 1 downto TAM_CUENTA) - CUENTA >= "01010" THEN
+                        CODE_SIG(1) <= PRECIOS((TAM_CUENTA*2) - 1 downto TAM_CUENTA) - "01010" - CUENTA;
+                    ELSE 
+                        CODE_SIG(1) <= PRECIOS((TAM_CUENTA*2) - 1 downto TAM_CUENTA) - CUENTA;
+                    END IF;
+                END IF;                      
+                --DP <= '1';
+            WHEN "111111011" =>
+                CONTROL_SIG(1) <= "111110110";
+                -- MOSTRAMOS LA UNIDAD DEL PRECIO (EN EUROS) QUE FALTA POR PAGAR 
+                -- SI TENEMOS QUE PAGAR MENOS DE UN EURO, SER� 0
+                IF TIPO_REFRESCO = "01" THEN
+                    IF PRECIOS(TAM_CUENTA - 1 downto 0) - CUENTA >= "01010" THEN
+                        CODE_SIG(1) <= "00001";
+                    ELSE CODE_SIG(1) <= "00000";
+                    END IF;
+                ELSIF TIPO_REFRESCO = "10" THEN
+                    IF PRECIOS((TAM_CUENTA*2) - 1 downto TAM_CUENTA) - CUENTA >= "01010" THEN
+                        CODE_SIG(1) <= "00001";
+                    ELSE CODE_SIG(1) <= "00000";
+                    END IF;
+                END IF;
+                --DP <= '0'; 
+            WHEN "111110110" =>
+                CONTROL_SIG(1) <= "111011111";
+                 -- Sacamos por pantalla el segundo decimal del precio del refresco: 0 siempre
+                CODE_SIG(1) <= "00000"; 
+                --DP <= '1';
+            WHEN "111011111" =>
+                CONTROL_SIG(1) <= "110111111";
+                IF TIPO_REFRESCO = "01" THEN
+                    -- Sacamos por pantalla el primer decimal del precio de 1.00� (10 - 10)
+                    CODE_SIG(1) <= PRECIOS(TAM_CUENTA - 1 downto 0) - "01010"; 
+                ELSIF TIPO_REFRESCO = "10" THEN
+                    -- Sacamos por pantalla el primer decimal del precio de 1.30� (13 - 10)
+                    CODE_SIG(1) <= PRECIOS((TAM_CUENTA*2) - 1 downto TAM_CUENTA)- "01010";
+                END IF;
+                --DP <= '1';
+            WHEN "110111111" =>
+                CONTROL_SIG(1) <= "101111110";
+                -- Sacamos por pantalla la unidad del precio del refresco: 1� siempre
+                CODE_SIG(1) <= "00001"; 
+                --DP <= '0';
+            WHEN OTHERS =>
+                CONTROL_SIG(1) <= "111111101";
+                CODE_SIG(1) <= "00000";
+                --DP <= '1';
+        END CASE;
+        
+        CASE CONTROL_SIG(2) IS -- EL REFRESCO HA SALIDO: OUT 1 / OUT 2
+            WHEN "111011111" =>
+                CONTROL_SIG(2) <= "110111111";
+                CODE_SIG(2) <= "11000"; -- LETRA T
+            WHEN "110111111" =>
+                CONTROL_SIG(2) <= "101111111";
+                CODE_SIG(2) <= "11001"; -- LETRA U
+            WHEN "101111111" =>
+                CONTROL_SIG(2) <= "011111111";
+                CODE_SIG(2) <= "10101"; -- LETRA O
+            WHEN OTHERS =>
+                CONTROL_SIG(2) <= "111011111";
+                IF TIPO_REFRESCO = "01" THEN
+                    CODE_SIG(2) <= "00001"; -- NUMERO DEL REFRESCO (1)      
+                ELSIF TIPO_REFRESCO = "10" THEN
+                    CODE_SIG(2) <= "00010"; -- NUMERO DEL REFRESCO (1) 
+                END IF; 
+        END CASE;
+        
+        CASE CONTROL_SIG(3) IS -- SE HA SOBREPASADO EL PRECIO DEL REFRESCO
+            WHEN "11101111"&'1' =>
+                CONTROL_SIG(3) <= "11011111"&'1';
+                CODE_SIG(3) <= "00001"; -- LETRA I/1
+            WHEN "11011111"&'1' =>
+                CONTROL_SIG(3) <= "10111111"&'1';
+                CODE_SIG(3) <= "10000"; -- LETRA A
+            WHEN "10111111"&'1' =>
+                CONTROL_SIG(3) <= "01111111"&'1';
+                CODE_SIG(3) <= "10011"; -- LETRA F
+            WHEN OTHERS =>
+                CONTROL_SIG(3) <= "11101111"&'1';
+                CODE_SIG(3) <= "10100"; -- LETRA L
+        END CASE;
+        
+    END IF; 
+    
+CONTROL <= CONTROL_SIG(3)&CONTROL_SIG(2)&CONTROL_SIG(1)&CONTROL_SIG(0);  
+CODE <= CODE_SIG(3)&CODE_SIG(2)&CODE_SIG(1)&CODE_SIG(0);   
+            
+end process;
+end Behavioral;
---
+++ MAQUINA_EXPENDEDORA/MAQUINA_EXPENDEDORA.srcs/sources_1/new/MAQ_ESTADOS.vhd
@@ -0,0 +1,101 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity FSM is
+    Generic(
+        NUM_TIPO_REFRESCOS: POSITIVE;
+        NUM_ESTADOS: POSITIVE;
+        NUM_DISPLAYS: POSITIVE;
+        TAM_CODE: POSITIVE           
+    );
+    Port( 
+        CLK : in STD_LOGIC;
+        PAGAR : in STD_LOGIC;
+        PAGO_CORRECTO : in STD_LOGIC;
+        TIPO_REFRESCO: in STD_LOGIC_VECTOR (NUM_TIPO_REFRESCOS - 1 downto 0);
+        ERROR_COUNTER : in STD_LOGIC;
+        ENTRADA_CONTROL : in STD_LOGIC_VECTOR (NUM_DISPLAYS * NUM_ESTADOS - 1 downto 0);
+        ENTRADA_CODE:in STD_LOGIC_VECTOR (TAM_CODE * NUM_ESTADOS - 1 downto 0);
+        RESET : in STD_LOGIC;
+        ERROR : out STD_LOGIC;
+        SALIDA_REFRESCO : out STD_LOGIC;
+        SALIDA_ESTADOS : out STD_LOGIC_VECTOR(NUM_ESTADOS - 1 downto 0);
+        SALIDA_CONTROL : out STD_LOGIC_VECTOR (NUM_DISPLAYS - 1 downto 0);
+        SALIDA_CODE: out STD_LOGIC_VECTOR (TAM_CODE - 1 downto 0)
+    );
+end FSM;
+
+architecture Behavioral of FSM is
+
+type STATES is (S0,S1,S2,S3);
+signal CURRENT_STATE: STATES := S0;
+signal NEXT_STATE: STATES;
+
+begin
+
+    state_register: process(RESET, CLK)
+    begin
+        if RESET = '0' then
+            CURRENT_STATE <= S0;
+        elsif rising_edge(CLK) then
+            CURRENT_STATE <= NEXT_STATE;
+        end if;
+    end process;
+    
+    nextstate: process(CLK) --, PAGAR, PAGO_OK, ERROR_COUNTER, CURRENT_STATE)
+    begin
+        NEXT_STATE <= CURRENT_STATE;
+        case CURRENT_STATE is 
+            when S0 =>
+                if PAGAR = '1' AND TIPO_REFRESCO /= "00" then
+                    NEXT_STATE <= S1;
+                end if;                
+            when S1 =>
+                if PAGO_CORRECTO = '1' then
+                    NEXT_STATE <= S2;
+                elsif ERROR_COUNTER = '1' then
+                    NEXT_STATE <= S3;
+                end if;                
+            when S2 =>
+                if PAGAR = '0' then
+                    NEXT_STATE <= S0;
+                end if;           
+            when S3 =>
+                if RESET = '0' then
+                    NEXT_STATE <= S0;
+                end if;            
+        end case;
+    end process;
+    
+    output_control: process(CLK, CURRENT_STATE)
+    begin
+        case CURRENT_STATE is 
+            when S0 =>
+                ERROR <= '0';
+                SALIDA_REFRESCO <= '0';
+                SALIDA_ESTADOS <= "0001";
+                SALIDA_CONTROL <= ENTRADA_CONTROL(NUM_DISPLAYS - 1 DOWNTO 0);
+                SALIDA_CODE <= ENTRADA_CODE(TAM_CODE - 1 DOWNTO 0);
+                
+            when S1 =>
+                ERROR <= '0';
+                SALIDA_REFRESCO <= '0';
+                SALIDA_ESTADOS <= "0010";
+                SALIDA_CONTROL<= ENTRADA_CONTROL((NUM_DISPLAYS * 2) - 1 DOWNTO NUM_DISPLAYS);
+                SALIDA_CODE <= ENTRADA_CODE((TAM_CODE * 2) - 1 DOWNTO TAM_CODE);
+            when S2 =>
+                ERROR <= '0';
+                SALIDA_REFRESCO <= '1';
+                SALIDA_ESTADOS <= "0100";
+                SALIDA_CONTROL <= ENTRADA_CONTROL((NUM_DISPLAYS * 3) - 1 DOWNTO NUM_DISPLAYS * 2);
+                SALIDA_CODE <= ENTRADA_CODE((TAM_CODE * 3) - 1 DOWNTO TAM_CODE * 2);
+            when S3 =>
+                ERROR <= '1';
+                SALIDA_REFRESCO <= '0';
+                SALIDA_ESTADOS <= "1000";
+                SALIDA_CONTROL <= ENTRADA_CONTROL((NUM_DISPLAYS * 4) - 1 DOWNTO NUM_DISPLAYS * 3);
+                SALIDA_CODE <= ENTRADA_CODE((TAM_CODE * 4) - 1 DOWNTO TAM_CODE * 3);
+        end case;
+    end process;
+
+end Behavioral;
